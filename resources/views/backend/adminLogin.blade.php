3024715f7d233eb4c8d41c1947b4e8752d9cabde
Slymee <jetli_rijan@hotmail.com>
2023-12-08 16:29:11 +0545

    Admin dashboard UI: complete

--- resources/views/backend/adminLogin.blade.php
+++ resources/views/backend/adminLogin.blade.php
@@ -5,18 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <title>Document</title>
+    <title>Dashboard-Login</title>
     @vite(['resources/css/admin-login.css'])
 
 </head>
 <body>
     <section class="main-section">
-        <div></div>
-        <div class="formBanner">
+        <div class="form-banner">
             <div class="img-place">
                 <img class="image" src="{{ URL('images/authenticate.jpg') }}" alt="Authenticate" srcset="">
             </div>
-            <div class="form-place">dfgfdgdfg</div>
+            <div class="form-place">
+                <span class="login-dashboard-text">Login to Dashboard</span>
+                <form action="" method="POST" autocomplete="off">
+                    <label for="username">Username</label><br>
+                    <input type="text" name="username" id="" placeholder="Enter Username">
+                    <br>
+                    <label for="password">Password</label><br>
+                    <input type="password" name="password" id="" placeholder="Enter Password"><br>
+                    <input type="submit" value="Login">
+                </form>
+                <span class="forgot-password">Forgot Password?</span>
+            </div>
         </div>
     </section>
 </body>
