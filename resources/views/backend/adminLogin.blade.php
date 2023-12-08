fa895dad8a2ca9b2dbc622828088d6179cc26f27
Slymee <jetli_rijan@hotmail.com>
2023-12-08 14:55:35 +0545

    designing backend admin UI: in progress

---
+++ resources/views/backend/adminLogin.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <title>Document</title>
+    @vite(['resources/css/admin-login.css'])
+
+</head>
+<body>
+    <section class="main-section">
+        <div></div>
+        <div class="formBanner">
+            <div class="img-place">
+                <img class="image" src="{{ URL('images/authenticate.jpg') }}" alt="Authenticate" srcset="">
+            </div>
+            <div class="form-place">dfgfdgdfg</div>
+        </div>
+    </section>
+</body>
+</html>
