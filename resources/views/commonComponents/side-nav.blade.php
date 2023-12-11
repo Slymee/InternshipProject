e365b9007d22968db210da56555cd191afd8d684
Slymee <jetli_rijan@hotmail.com>
2023-12-11 14:01:00 +0545

    Admin dashboard UI: WIP

--- resources/views/commonComponents/side-nav.blade.php
+++ resources/views/commonComponents/side-nav.blade.php
@@ -10,14 +10,14 @@
                 <li><i class="fa-solid fa-layer-group"></i></i> Sub-category</li>
                 <li><i class="fa-solid fa-layer-group"></i> Product</li>
                 <li><i class="fa-solid fa-layer-group"></i></i> Orders</li>
-                <li><i class="fa-solid fa-layer-group"></i></i> Category</li>
+                <li><i class="fa-solid fa-layer-group"></i></i> Users</li>
+                <li><i class="fa-solid fa-layer-group"></i></i> Admins</li>
 
             </ul>
         </div>
         <div class="utilities">
             <ul>
-                <li><i class="fa-solid fa-layer-group"></i></i> Admins</li>
-                <li><i class="fa-solid fa-layer-group"></i></i> Users</li>
+                <li><i class="fa-solid fa-layer-group"></i></i> Settings</li>
                 <li><i class="fa-solid fa-layer-group"></i></i> Logout</li>
             </ul>
         </div>
