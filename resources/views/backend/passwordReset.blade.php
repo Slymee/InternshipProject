b0df8fe08dcfcfecd2ee73c388f215c7a20c8135
Slymee <jetli_rijan@hotmail.com>
2023-12-26 07:41:19 +0545

    Admin authentication layout editing: complete

--- resources/views/backend/passwordReset.blade.php
+++ resources/views/backend/passwordReset.blade.php
@@ -1,51 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+@extends('backend.layouts.login-template')
 
-    <title>Forgot Password</title>
-    @vite(['resources/css/admin-login.css'])
-</head>
-<body>
-    <section class="main-section">
-        <div class="form-banner">
-            <div class="img-place">
-                <img class="image" src="{{ URL('images/authenticate.jpg') }}" alt="Authenticate" srcset="">
-            </div>
-            <div class="form-place">
-                <span class="login-dashboard-text">Reset Password</span>
-                <form action={{ route{{ 'admin.new.password' }} }} method="POST" autocomplete="off">
-                    @csrf
-                    <label for="new-password">New Password</label><br>
-                    <input type="password" name="new-password" id="" placeholder="Enter New Password">
-                    <br>
-                    <label for="confirm-password">Confirm Password</label><br>
-                    <input type="password" name="confirm-password" id="" placeholder="Confirm New Password">
+@section('content')
+<div class="form-place">
+    <span class="login-dashboard-text">Reset Password</span>
+    <form action={{ route('admin.new.password') }} method="POST" autocomplete="off">
+        @csrf
+        <label for="new-password">New Password</label><br>
+        <input type="password" name="new-password" id="" placeholder="Enter New Password">
+        <br>
+        <label for="confirm-password">Confirm Password</label><br>
+        <input type="password" name="confirm-password" id="" placeholder="Confirm New Password">
 
-                    <input type="hidden" name="token" value="{{ $token }}">
-                    
-                    <input type="submit" value="Confirm Password">
+        <input type="hidden" name="token" value="{{ $token }}">
+        
+        <input type="submit" value="Confirm Password">
 
-                    
-                </form>
-                <span class="error-message">
-                @if(session('message'))
-                    {{ session('message') }}
-                @endif
+        
+    </form>
+    <span class="error-message">
+    @if(session('message'))
+        {{ session('message') }}
+    @endif
 
-                @if($errors->any())
-                    @foreach ($errors->all() as $error)
-                        {{ $error}} <br>
-                    @endforeach                    
-                @endif
-                </span>
+    @if($errors->any())
+        @foreach ($errors->all() as $error)
+            {{ $error}} <br>
+        @endforeach                    
+    @endif
+    </span>
 
-            </div>
-        </div>
-    </section>
-
-</body>
-</html>
+</div>    
+@endsection
